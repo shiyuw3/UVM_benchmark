0714bfa0084af1e16998b5a7126ea9e7bffe7f7b
root <wangshiyu123@hotmail.com>
2022-05-15 09:03:01 +0800

    feat(GESUMMV): add polybench GESUMMV testcase

--- UVM_benchmarks/polybench/GESUMMV/gesummv.cu
+++ UVM_benchmarks/polybench/GESUMMV/gesummv.cu
@@ -24,7 +24,7 @@
 #define GPU_DEVICE 0
 
 /* Problem size */
-#define N 1200
+#define N 300
 
 /* Thread block dimensions */
 #define DIM_THREAD_BLOCK_X 256
@@ -34,7 +34,6 @@
    (same as values in PolyBench 2.0) */
 #define ALPHA 43532.0f
 #define BETA 12313.0f
-#define ENABLE_PREFETCH 0
 
 /* Can switch DATA_TYPE between float and double */
 typedef float DATA_TYPE;
@@ -108,52 +107,14 @@ __global__ void gesummv_kernel(DATA_TYPE *a, DATA_TYPE *b, DATA_TYPE *x,
 
 void gesummvCuda(DATA_TYPE* A_gpu, DATA_TYPE* B_gpu, DATA_TYPE* x_gpu,
                  DATA_TYPE* y_gpu, DATA_TYPE* tmp_gpu) {
-#if ENABLE_PREFETCH
-  cudaStream_t stream1;
-  cudaStream_t stream2;
-  cudaStream_t stream3;
-  cudaStream_t stream4;
-  cudaStream_t stream5;
-  cudaStreamCreate(&stream1);
-  cudaStreamCreate(&stream2);
-  cudaStreamCreate(&stream3);
-  cudaStreamCreate(&stream4);
-  cudaStreamCreate(&stream5);
-
-  double t_start, t_end;
-  dim3 block(DIM_THREAD_BLOCK_X, DIM_THREAD_BLOCK_Y);
-  dim3 grid((unsigned int)ceil(((float)N) / ((float)block.x)), 1);
-  cudaMemPrefetchAsync(A_gpu, N * N * sizeof(DATA_TYPE), GPU_DEVICE, stream1);
-  cudaMemPrefetchAsync(B_gpu, N * N * sizeof(DATA_TYPE), GPU_DEVICE, stream2);
-  cudaMemPrefetchAsync(x_gpu, N * sizeof(DATA_TYPE), GPU_DEVICE, stream3);
-  cudaMemPrefetchAsync(y_gpu, N * sizeof(DATA_TYPE), GPU_DEVICE, stream4);
-  cudaMemPrefetchAsync(tmp_gpu, N * sizeof(DATA_TYPE), GPU_DEVICE, stream5);
-  cudaStreamSynchronize(stream1);
-  cudaStreamSynchronize(stream2);
-  cudaStreamSynchronize(stream3);
-  cudaStreamSynchronize(stream4);
-  cudaStreamSynchronize(stream5);
-  t_start = rtclock();
-  for (int i = 0; i < 1; i++) {
-    gesummv_kernel<<<grid, block, 0, stream5>>>(
-        A_gpu, B_gpu, x_gpu, y_gpu, tmp_gpu);
-    cudaDeviceSynchronize();
-  }
-  t_end = rtclock();
-  fprintf(stdout, "GPU Runtime: %0.6lfs\n", t_end - t_start);
-
-#else
   double t_start, t_end;
   dim3 block(DIM_THREAD_BLOCK_X, DIM_THREAD_BLOCK_Y);
   dim3 grid((unsigned int)ceil(((float)N) / ((float)block.x)), 1);
   t_start = rtclock();
-  for (int i = 0; i < 1; i++) {
-    gesummv_kernel<<<grid, block>>>(A_gpu, B_gpu, x_gpu, y_gpu, tmp_gpu);
-    cudaDeviceSynchronize();
-  }
+  gesummv_kernel<<<grid, block>>>(A_gpu, B_gpu, x_gpu, y_gpu, tmp_gpu);
+  cudaDeviceSynchronize();
   t_end = rtclock();
   fprintf(stdout, "GPU Runtime: %0.6lfs\n", t_end - t_start);
-#endif
 }
 
 int main(int argc, char *argv[]) {
